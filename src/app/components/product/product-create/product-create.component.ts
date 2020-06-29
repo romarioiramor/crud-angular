c2f3ccaaa119b9bb5ef5860f8f478871454f2def
Romario Medeiros <44781491+romarioiramor@users.noreply.github.com>
2020-06-29 13:27:39 -0300

    Add files via upload

---
+++ src/app/components/product/product-create/product-create.component.ts
@@ -0,0 +1,38 @@
+
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+
+import { ProductService } from './../product.service';
+import { Product } from './../product.model';
+
+@Component({
+  selector: 'app-product-create',
+  templateUrl: './product-create.component.html',
+  styleUrls: ['./product-create.component.css']
+})
+export class ProductCreateComponent implements OnInit {
+
+  product: Product = {
+    name: '',
+    price: null
+  }
+
+  constructor(private ProductService: ProductService,
+    private router: Router) { }
+
+  ngOnInit(): void {
+
+  }
+
+  createProduct(): void {
+    this.ProductService.create(this.product).subscribe(() => {
+      this.ProductService.showMessage('Produto criado!')
+      this.router.navigate(['/products'])
+    })
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products'])
+  }
+
+}
